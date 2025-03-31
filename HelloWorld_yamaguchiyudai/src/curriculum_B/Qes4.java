071f52da2202d90d8742997e22d97b684f972c75
yamad <yamad@DESKTOP-RVACKDD>
2025-03-31 17:21:53 +0900

    課題４

--- HelloWorld_yamaguchiyudai/src/curriculum_B/Qes4.java
+++ HelloWorld_yamaguchiyudai/src/curriculum_B/Qes4.java
@@ -3,8 +3,19 @@ package curriculum_B;
 public class Qes4 {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
-
+		
+		// 九九の計算
+		for (int i = 1; i <= 9; i++) {
+            for (int j = 1; j <= 9; j++) {
+                
+        // 出力
+         System.out.printf("│%02d×%02d=%02d│", i, j, i * j);
+            }
+		
+        // 段ごとに改行
+		 System.out.println();
+		           
+		}
+      }
 	}
-
-}
+		 
